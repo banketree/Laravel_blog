1a8f192a6e8584930aa3198cd3954766c4f44b73
Banketree <banketree@qq.com>
2017-07-02 15:29:18 +0800

    step-40

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,7 @@
 */
 
 Route::group(['middleware' => ['web']], function () {
+
     Route::get('/', function () {
         return view('welcome');
     });
@@ -29,4 +30,9 @@ Route::group(['middleware' => ['web','admin.login'],'prefix'=>'admin','namespace
 
     Route::post('cate/changeorder', 'CategoryController@changeOrder');
     Route::resource('category', 'CategoryController');
+
+    Route::resource('article', 'ArticleController');
+
+    Route::any('upload', 'CommonController@upload');
+
 });
